bc89e2537b2370b88838c912e5711a909bedfa8c
Manish DEV <manish1771999@gmail.com>
2020-10-11 01:44:25 +0530

    Transaction conflicts

--- BackEnd/models/transaction.js
+++ BackEnd/models/transaction.js
@@ -2,31 +2,6 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-<<<<<<< HEAD
-const transactionSchema = new Schema({
-  user_id: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-    min: 3,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now(),
-  },
-});
-=======
 const transactionSchema = new Schema(
   {
     user_id: {
@@ -55,6 +30,5 @@ const transactionSchema = new Schema(
     versionKey: false,
   }
 );
->>>>>>> origin
 
 module.exports = mongoose.model("UserTransaction", transactionSchema);

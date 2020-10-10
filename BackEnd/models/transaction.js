dd92b8f4236412078186b766015eed06867daf0d
Manish DEV <manish1771999@gmail.com>
2020-10-11 01:30:41 +0530

    Removed Version Key

--- BackEnd/models/transaction.js
+++ BackEnd/models/transaction.js
@@ -1,29 +1,34 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
+const transactionSchema = new Schema(
+  {
     user_id: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     title: {
-        type: String,
-        required: true,
-        min: 3
+      type: String,
+      required: true,
+      min: 3,
     },
     type: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     amount: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     date: {
-        type: Date,
-        default: Date.now()
-    }
-})
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-module.exports = mongoose.model('UserTransaction', transactionSchema)
+module.exports = mongoose.model("UserTransaction", transactionSchema);

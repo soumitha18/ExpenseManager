0c6e27d963f6e7ebbbd334b8998839f3cb7ae914
Manish DEV <manish1771999@gmail.com>
2020-10-14 01:36:37 +0530

    Added Material-UI Dashboard Cards and Recent Transactions

--- FrontEnd/expense-manager/src/Pages/Dashboard.jsx
+++ FrontEnd/expense-manager/src/Pages/Dashboard.jsx
@@ -7,14 +7,18 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useHistory, Link } from "react-router-dom";
 import styled from "styled-components";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import RecentTransactions from "../Components/RecentTransactions";
 
 const DashboardWrapper = styled.div`
   .visual {
-    border: 1px solid black;
     height: 100vh;
   }
-
+  background-color: #f5f7f6;
   .sidebar {
+    background-color: white;
+    box-shadow: 5px 5px 10px #e6e6e6;
     h4 {
       text-align: center;
       font-family: "Poppins";
@@ -26,7 +30,7 @@ const DashboardWrapper = styled.div`
     .sidebarDivItems {
       margin: 35px 0px;
       a {
-        color: #678c7f;
+        color: #8f8f8f;
         text-decoration: none;
         :hover {
           color: black;
@@ -46,7 +50,7 @@ const DashboardWrapper = styled.div`
         button {
           border: none;
           background-color: transparent;
-          color: #678c7f;
+          color: #8f8f8f;
           padding-right: 105px;
           outline: none;
           :hover {
@@ -61,20 +65,101 @@ const DashboardWrapper = styled.div`
       }
     }
   }
+
+  .userNameDiv {
+    font-family: "Poppins";
+    ${"" /* Capitalises the first letter */}
+    text-transform: capitalize;
+    margin: 20px;
+  }
+
+  .balanceDashboardCardBody {
+    display: flex;
+    align-items: center;
+
+    .balanceDashboardCardTitle {
+      flex: 1;
+      color: #8f8f8f;
+      margin: auto;
+      font-family: "Poppins";
+    }
+
+    .balanceDashboardCardText {
+      font-family: "Poppins";
+      font-size: 30px;
+    }
+  }
+
+  .totalBalance,
+  .totalIncome {
+    border: 1px solid #d4d4d4;
+    :hover {
+      transition: 0.2s;
+      border: 1px solid #10b26c;
+      box-shadow: 5px 5px 10px #d3d4d4;
+      .balanceDashboardCardTitle {
+        transition: 0.2s;
+        color: black;
+      }
+
+      .balanceDashboardCardText {
+        transition: 0.2s;
+        color: #10b26c;
+      }
+    }
+  }
+
+  .totalExpense {
+    border: 1px solid #d4d4d4;
+    :hover {
+      transition: 0.2s;
+      border: 1px solid red;
+      box-shadow: 5px 5px 10px #d3d4d4;
+      .balanceDashboardCardTitle {
+        transition: 0.2s;
+        color: black;
+      }
+
+      .balanceDashboardCardText {
+        transition: 0.2s;
+        color: #ff601c;
+      }
+    }
+  }
+
+  .recentTransactionsDiv {
+    margin: 20px 10px;
+    > h5 {
+      font-family: "Poppins";
+      margin: 15px 0px;
+    }
+  }
 `;
 
 export default function Dashboard() {
   const history = useHistory();
   const userData = JSON.parse(localStorage.getItem("activeUserDetails"));
+  const [isLoading, setIsLoading] = useState(true);
   const [recentTransactions, setRecentTransactions] = useState([]);
   const [totalIncome, setTotalIncome] = useState(0);
   const [totalExpense, setTotalExpense] = useState(0);
   const [totalBalance, setTotalBalance] = useState(0);
 
+  //function to convert a number to a local currency format
+  const indianCurrencyFormat = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+  });
+
   useEffect(() => {
     axios
       .get(`http://localhost:5000/user/transactions?user=${userData._id}`)
-      .then((res) => console.log(res))
+      .then((res) => {
+        setRecentTransactions(res.data.transaction);
+        setTotalBalance(res.data.balance);
+        setTotalIncome(res.data.total_income);
+        setTotalExpense(res.data.total_expense);
+      })
       .catch((err) => console.log(err.response.data));
   }, []);
 
@@ -83,7 +168,7 @@ export default function Dashboard() {
       method: "post",
       url: "http://localhost:5000/user/logout",
       data: {
-        email: userData.email
+        email: userData.email,
       },
     })
       .then((response) => {
@@ -119,8 +204,9 @@ export default function Dashboard() {
             </Col>
           </Row>
           <Row>
+            {/* The Sidebar division */}
             <Col className="visual d-none d-lg-block sidebar" lg={2}>
-              <h4>EXPENSE MANAGER</h4>
+              <h4 style={{ color: "#0AC76F" }}>EXPENSE MANAGER</h4>
               <div className="sidebarDivItems">
                 <ul style={{ listStyle: "none" }}>
                   <Link to="/dashboard">
@@ -141,7 +227,58 @@ export default function Dashboard() {
                 </ul>
               </div>
             </Col>
-            <Col className="visual" lg={10}></Col>
+
+            {/* The main Dashboard Division*/}
+            <Col className="visual" lg={10}>
+              {/*Dashboard Card Div */}
+              <Row>
+                <Col sm={12}>
+                  <h1 className="userNameDiv">Hello, {userData.name}</h1>
+                </Col>
+                <Col>
+                  <CardDeck>
+                    <Card className="totalBalance">
+                      <Card.Body className="balanceDashboardCardBody">
+                        <Card.Title className="balanceDashboardCardTitle">
+                          TOTAL BALANCE
+                        </Card.Title>
+                        <Card.Text className="balanceDashboardCardText">
+                          {indianCurrencyFormat.format(totalBalance)}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                    <Card className="totalIncome">
+                      <Card.Body className="balanceDashboardCardBody">
+                        <Card.Title className="balanceDashboardCardTitle">
+                          TOTAL INCOME
+                        </Card.Title>
+                        <Card.Text className="balanceDashboardCardText">
+                          {indianCurrencyFormat.format(totalIncome)}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                    <Card className="totalExpense">
+                      <Card.Body className="balanceDashboardCardBody">
+                        <Card.Title className="balanceDashboardCardTitle">
+                          TOTAL EXPENSE
+                        </Card.Title>
+                        <Card.Text className="balanceDashboardCardText">
+                          {indianCurrencyFormat.format(totalExpense)}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </CardDeck>
+                </Col>
+              </Row>
+              {/* Dashboard Recent 5 Transactions, and Enter Transaction Form Div */}
+              <Row>
+                <Col className="recentTransactionsDiv" lg={5}>
+                  <h5>Recent Transactions</h5>
+                  <RecentTransactions data={recentTransactions} />
+                </Col>
+                <Col lg={7}></Col>
+              </Row>
+            </Col>
           </Row>
         </Container>
       </DashboardWrapper>
